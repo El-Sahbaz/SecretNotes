d7415fc52caa1feadce9e78e19c239cc1072e815
Emre Şahbaz <el.sahbaz.career@gmail.com>
2025-07-13 00:45:06 +0300

    decrypt,encrypt added

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from tkinter import *
 from tkinter import messagebox
+import base64
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
 
 def save_and_encrypt_notes():
     title = title_entry.get()
@@ -10,18 +29,28 @@ def save_and_encrypt_notes():
         messagebox.showinfo(title="Error!",message="Please enter all information.")
     else:
         #encryption
+        message_encrypted = encode(master_secret,message)
         try:
             with open("mysecret.txt","a") as data_file:
-                data_file.write(f"\n{title}\n{message}")
+                data_file.write(f"\n{title}\n{message_encrypted}")
         except FileNotFoundError:
             with open("mysecret.txt","w") as data_file:
-                data_file.write(f"\n{title}\n{message}")
+                data_file.write(f"\n{title}\n{message_encrypted}")
         finally:
             title_entry.delete(0,END)
             master_secret_input.delete(0,END)
             input_text.delete("1.0",END)
 
+def decrypt_notes():
+    message_encrypted = input_text.get("1.0",END)
+    master_secret = master_secret_input.get()
 
+    if len(message_encrypted) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="Error!",message="Please enter all info")
+    else:
+        decrypted_message = decode(master_secret,message_encrypted)
+        input_text.delete("1.0",END)
+        input_text.insert("1.0",decrypted_message)
 
 #UI
 
